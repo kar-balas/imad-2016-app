3e74a9380d83127cba3b48dde9d50290d582e4a9
kar-balas <karthik.kumar.tcb@gmail.com>
2016-10-21 12:27:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,5 @@
 console.log('Loaded!');
 // chanfe the text of main file div
 var element= document.getElementById('main_text');
-element.InnerHTML="New value";
+//element.InnerHTML="New value";
+element.innerHTML="Hi";

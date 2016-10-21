05259f9d10711e74e993948dbb21c0f97e3d4168
kar-balas <karthik.kumar.tcb@gmail.com>
2016-10-21 09:59:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui','madi.png'));
 });
 
 app.get ('/:articleName',function(req,res){

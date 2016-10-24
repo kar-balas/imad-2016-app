5ff9d44415fd677be9ba91d15fdf7c82959515a8
kar-balas <karthik.kumar.tcb@gmail.com>
2016-10-24 13:04:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,7 @@ var element= document.getElementById('main_text');
 element.innerHTML="New value";
 var img = document.getElementById("madi");
 console.log('Loaded2');
+console.log(img);
  img.onClick = function(){
     console.log("Inside OnClick");
    img.style.marginLeft = marginLeft+ '100px';

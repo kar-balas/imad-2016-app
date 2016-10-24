cfa8e3f219172da37abef9521368c67459ef651f
kar-balas <karthik.kumar.tcb@gmail.com>
2016-10-24 17:23:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,14 +3,14 @@ console.log('Loaded!');
 var element= document.getElementById('main_text');
 element.innerHTML="New value";
 var img = document.getElementById('madi');
-var marginleft=0;
-function  moveright(){
-    marginleft=marginleft+10;
- img.style.marginLeft = marginleft +'px';   
+var marginLeft=0;
+function  moveRight(){
+    marginLeft=marginLeft+10;
+ img.style.marginLeft = marginLeft +'px';   
     
 }
 
  img.onclick = function(){
-   var interval =  setinterval(moveright,100);
+   var interval =  setInterval(moveRight,100);
  
 };

a7d47fa2bfbc0501f74d4ebe9d83f1d4f0fc4fc8
kar-balas <karthik.kumar.tcb@gmail.com>
2016-10-26 00:28:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,12 +6,12 @@ var img = document.getElementById('madi');
 var marginLeft=0;
 var i;
 function  moveRight(){
-    marginLeft=marginLeft+10;
+    marginLeft=marginLeft+5;
  img.style.marginLeft = marginLeft +'px';   
     
 }
 
  img.onclick = function(){
      
-   var interval =  setInterval(moveRight,100);
+   var interval =  setInterval(moveRight,50);
 };

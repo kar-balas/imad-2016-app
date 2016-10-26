9127316036648fc4f417c0fc7b2110670b06d707
kar-balas <karthik.kumar.tcb@gmail.com>
2016-10-26 11:47:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,12 +100,12 @@ app.get('/ui/favicon.ico', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
 });
 
-//var counter=0;
-//app.get('/ui/counter', function (req, res) {
-//counter = counter + 1;
- // res.send(counter.toString());
+var counter=0;
+app.get('/ui/counter', function (req, res) {
+counter = counter + 1;
+ res.send(counter.toString());
     
-//});
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui','madi.png'));

a9bc76de1119ef4e2949ada8fd8934e63ae6f791
kar-balas <karthik.kumar.tcb@gmail.com>
2016-10-27 11:16:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,17 @@
 // create a request
+//var request=new XMLHttpRequest
 //
 //make a request
+
+
+
+
+
 //execute what u want to do
 var ctr =0;
 var ctrButton=document.getElementById('ctrBtn');
 ctrButton.onclick=function(){
 ctr += 1;
 var element=document.getElementById('counter');
-console.log("incremented value is:");
-console.log(ctr);
 element.innerHTML=ctr.toString();
-console.log("inner html is:");
-console.log(element.innerHTML);
-    
 };
-
-ctr=ctr+1;

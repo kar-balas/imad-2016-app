9a24334a3480071268c4f6850d2cd18089cbcdf4
kar-balas <karthik.kumar.tcb@gmail.com>
2016-10-27 11:01:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ ctrButton.onclick=function(){
 ctr += 1;
 var element=document.getElementById('counter');
 console.log("incremented value is:");
-console.log('ctr');
+console.log(ctr);
 element.innerHtml=ctr.toString();
     
 };

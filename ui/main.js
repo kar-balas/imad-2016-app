2456ca3cb47e8a0bf680b71c4fdeba23320f0626
kar-balas <karthik.kumar.tcb@gmail.com>
2016-10-27 10:56:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,26 +1,14 @@
-console.log('Loaded!');
-// chanfe the text of main file div
-var element= document.getElementById('main_text');
-element.innerHTML="New value from localhost";
-var img = document.getElementById('madi');
-var marginLeft=0;
-var i;
-function  moveRight(){
-    marginLeft=marginLeft+5;
- img.style.marginLeft = marginLeft +'px';   
+// create a request
+//
+//make a request
+//execute what u want to do
+var ctr =0;
+var ctrButton=document.getElementById('ctrBtn');
+ctrButton.onclick=function(){
+ctr += 1;
+var element=document.getElementById('counter');
+element.innerHtml=ctr.toString();
     
-}
-
- img.onclick = function(){
-     
-   var interval =  setInterval(moveRight,50);
 };
 
-var ctrbutton=document.getElementById('ctrBtn');
-counter=0;
-ctrbutton.onclick = function(){
-    counter+=1;
- element=document.getElementById('counter');   
- element.innerHTML=counter.toString();
-};
-
+ctr=ctr+1;

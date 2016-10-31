46f6311ed2e5dfa79d2a766ec3c942ab4291648f
kar-balas <karthik.kumar.tcb@gmail.com>
2016-10-31 08:59:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,9 @@
 //execute what u want to do
 var ctr =0;
 var ctrButton=document.getElementById('ctrBtn');
+    log.console('Outside Button Click');
 ctrButton.onclick=function(){
+    log.console('Inside Button Click');
 ctr += 1;
 var element=document.getElementById('counter');
 element.innerHTML=ctr.toString();
